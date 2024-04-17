d95e24f37f75116ac865f15224a5a1cf037fe9a7
maissabma <maissa@unfrauded.com>
2024-04-17 17:20:46 +0100

    modifs

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerJsdoc = require('swagger-jsdoc')
 const cors = require('cors')
 const morgan = require('morgan')
-const authorityRoute =require('./routes/authorityRoute');
-const daaqRoute = require('./routes/daaqRoute');
-const userGroupRoute = require('./routes/userGroupRoute');
-const userRoute = require('./routes/userRoute');
-const teamRoute = require('./routes/teamRoute');
+const passport = require('passport');
 require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+app.use(passport.initialize());
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -25,14 +21,17 @@ const swaggerOptions = {
   },
   apis: ['./routes/*.js'],
 }
-
 const swaggerSpec = swaggerJsdoc(swaggerOptions)
-
 const port = process.env.PORT || 4000
 const dbConnectionString = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_IP}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
 app.use(morgan('combined'))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.listen(port, () => console.log(`ms-user is running on port ${port}`))
+const authorityRoute =require('./routes/authorityRoute');
+const daaqRoute = require('./routes/daaqRoute');
+const userGroupRoute = require('./routes/userGroupRoute');
+const userRoute = require('./routes/userRoute');
+const teamRoute = require('./routes/teamRoute');
 app.use('/api/v0.1', authorityRoute);
 app.use('/api/v0.1', daaqRoute);
 app.use('/api/v0.1', userGroupRoute);
